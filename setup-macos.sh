a1598f74a41de25a6d274a827d7805bffa342784
Brendan Carlson <brendancarlson1125@gmail.com>
2024-03-31 11:45:55 -0500

    fix: setup script now restows instead of default stow

--- setup-macos.sh
+++ setup-macos.sh
@@ -5,5 +5,5 @@ stowpackages="git,nvim,ohmyzsh,scripts,tmux,zsh"
 IFS="," read -r -a stowarray <<< "$stowpackages"
 
 for package in "${stowarray[@]}"; do
-    stow $package
+    stow -R $package
 done
