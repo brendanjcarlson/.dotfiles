21c4cd765d70fc37f90d146b649e52a726a7ce65
Brendan Carlson <brendancarlson1125@gmail.com>
2024-04-15 08:40:46 -0500

    changed colorscheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,7 +36,7 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv") -- in visual mode, move selected li
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv") -- in visual mode, move selected lines up
 vim.keymap.set('n', '<C-u', '<C-u>zz') -- recenter viewport on half page jumps
 vim.keymap.set('n', '<C-d', '<C-d>zz') -- recenter viewport on half page jumps
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]]) -- yank to system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+Y]]) -- yank to system clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>Y', [["+Y]])
 vim.keymap.set('i', '<C-c>', '<Esc>') -- idk
 vim.keymap.set('n', 'Q', '<nop>') -- apparently cap q is the worst?
@@ -594,7 +594,27 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight'
+      -- NOTE: UNCOMMENT TO SWITCH BACK
+      -- vim.cmd.colorscheme 'tokyonight'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'rose-pine-moon'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -684,12 +704,12 @@ require('lazy').setup({
     'tpope/vim-fugitive', -- better git
   },
 
-  {
-    'folke/trouble.nvim', -- better diagnostics
-    config = function()
-      vim.keymap.set('n', '<leader>tt', '<cmd>TroubleToggle quickfix<CR>')
-    end,
-  },
+  -- {
+  --   'folke/trouble.nvim', -- better diagnostics
+  --   config = function()
+  --     vim.keymap.set('n', '<leader>tt', '<cmd>TroubleToggle quickfix<CR>')
+  --   end,
+  -- },
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
